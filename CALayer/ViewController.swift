616ccb172951146778acd7ea4519e5b799610432
cv0cv0 <linux2.0@foxmail.com>
2019-03-10 11:19:20 +0800

    Initial

--- CALayer/ViewController.swift
+++ CALayer/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let slider = UISlider()
+    let progressLayers = Array(repeating: ProgressLayer(), count: 4)
+    
+    let top = 110
+    let vSpace = 30
+    let size = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        slider.addTarget(self, action: #selector(valueChanged), for: .valueChanged)
+        view.addSubview(slider)
+        
+        progressLayers.forEach { progressLayer in
+            view.layer.addSublayer(progressLayer)
+        }
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let width = Int(view.frame.width)
+        slider.frame = CGRect(x: 50, y: 70, width: width - 100, height: 30)
+        
+        let hSpace = (width - size * 2) / 3
+        for (index, progressLayer) in progressLayers.enumerated() {
+            progressLayer.frame = CGRect(x: hSpace + (size + hSpace) * (index % 2), y: <#T##Int#>, width: size, height: size)
+        }
+    }
+    
+    @objc func valueChanged() {
+        
+    }
 }
 
+class ProgressLayer: CALayer {
+    let textLayer = CATextLayer()
+    
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
